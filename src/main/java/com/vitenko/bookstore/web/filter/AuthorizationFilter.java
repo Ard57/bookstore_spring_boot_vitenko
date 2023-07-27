651163cc2d024d995be0f273b160ad4a5a551b4b
Ard57 <grunvald.by@gmail.com>
2023-07-27 15:02:37 +0300

    Update filter classes

--- src/main/java/com/vitenko/bookstore/web/filter/AuthorizationFilter.java
+++ src/main/java/com/vitenko/bookstore/web/filter/AuthorizationFilter.java
@@ -2,13 +2,11 @@ package com.vitenko.bookstore.web.filter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = {"/user/*", "/order/*", "/logout", "/cart/purchase"})
 public class AuthorizationFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
