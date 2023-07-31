bf2c23c0472245ae83f39a230475a2b4d66edf8d
Ard57 <grunvald.by@gmail.com>
2023-07-31 16:10:54 +0300

    Update CartController
    Remove server side rendering for cart's content
    Move /cart/add/ request to REST controller

--- src/main/java/com/vitenko/bookstore/web/controller/view/CartController.java
+++ src/main/java/com/vitenko/bookstore/web/controller/view/CartController.java
@@ -1,10 +1,7 @@
 package com.vitenko.bookstore.web.controller.view;
 
-import com.vitenko.bookstore.exception.book.BookNotFoundException;
 import com.vitenko.bookstore.service.CartService;
 import com.vitenko.bookstore.service.dto.OrderDto;
-import com.vitenko.bookstore.service.dto.OrderItemDto;
-import com.vitenko.bookstore.service.dto.UserDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.ui.Model;
@@ -13,7 +10,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpSession;
-import java.math.BigDecimal;
 
 @Log4j2
 @RequiredArgsConstructor
@@ -22,63 +18,9 @@ public class CartController {
     private final CartService cartService;
 
     @GetMapping(path = "/cart")
-    public String getCart(Model model, HttpSession session) {
-        OrderDto cart = (OrderDto) session.getAttribute("cart");
-        if (cart == null) {
-            cart = cartService.createCart();
-            if (session.getAttribute("user") != null) {
-                cartService.setUser(cart, (UserDto) session.getAttribute("user"));
-            }
-            session.setAttribute("cart", cart);
-        }
-
-
-        BigDecimal totalPrice = new BigDecimal(0);
-        for (OrderItemDto orderItemDto : cart.getOrderItems()) {
-            totalPrice = totalPrice.add(
-                    orderItemDto.getPrice().multiply(
-                            new BigDecimal(orderItemDto.getAmount())));
-        }
-        model.addAttribute("totalPrice", totalPrice);
-
-        int cartSize = cartService.calculateCartSize(cart);
-        session.setAttribute("cartSize", cartSize);
-
-        if (session.getAttribute("user") == null) {
-            model.addAttribute("purchasePossible", false);
-            model.addAttribute("purchaseHelp", "Please, log in to make an order");
-        } else if (cartSize < 1) {
-            model.addAttribute("purchasePossible", false);
-            model.addAttribute("purchaseHelp", "Please, make sure your cart isn't empty to make an order");
-        } else {
-            model.addAttribute("purchasePossible", true);
-            model.addAttribute("purchaseHelp", "");
-        }
-
+    public String getCart(Model model) {
         model.addAttribute("title", "Cart");
-        model.addAttribute("orderDto", cart);
         return "order/cart";
-    }
-
-    @RequestMapping(path = "/cart/add/{bookId}")
-    public RedirectView addToCart(@PathVariable Long bookId,
-                                  @RequestParam(value = "amount", required = false) Integer amount,
-                                  HttpSession session, Model model) throws BookNotFoundException {
-        OrderDto cart = (OrderDto) session.getAttribute("cart");
-        if (cart == null) {
-            cart = cartService.createCart();
-            session.setAttribute("cart", cart);
-        }
-
-        if (amount == null) {
-            amount = 1;
-        }
-
-        cart = cartService.addItem(cart, bookId, amount);
-
-        model.addAttribute("orderDto", cart);
-
-        return new RedirectView("/cart");
     }
 
     @PostMapping(path = "/cart/purchase")
