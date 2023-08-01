aee8b9ffc417ab607d540b0ab580be457a35d532
Ard57 <grunvald.by@gmail.com>
2023-08-01 14:30:33 +0300

    Update BookRestController class
    Add methods for creating, updating and deleting books

--- src/main/java/com/vitenko/bookstore/web/controller/rest/BookRestController.java
+++ src/main/java/com/vitenko/bookstore/web/controller/rest/BookRestController.java
@@ -1,27 +1,45 @@
 package com.vitenko.bookstore.web.controller.rest;
 
 import com.vitenko.bookstore.exception.book.BookNotFoundException;
+import com.vitenko.bookstore.exception.book.IllegalBookArgumentException;
 import com.vitenko.bookstore.service.BookService;
 import com.vitenko.bookstore.service.dto.BookDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/books")
 public class BookRestController {
     private final BookService bookService;
 
-    @GetMapping("/api/books/{id}")
+    @GetMapping("/{id}")
     public BookDto getBook(@PathVariable Long id) throws BookNotFoundException {
         return bookService.findById(id);
     }
 
-    @GetMapping("/api/books/all")
+    @GetMapping("/all")
     public List<BookDto> getAllBooks() {
         return bookService.getAllBooks();
+    }
+
+    @PostMapping("")
+    public BookDto createBook(@ModelAttribute("bookDto") BookDto bookDto)
+            throws IllegalBookArgumentException {
+        return bookService.create(bookDto);
+    }
+
+    @PutMapping("/{id}")
+    public BookDto updateBook(@PathVariable Long id, @ModelAttribute("bookDto") BookDto bookDto)
+            throws IllegalBookArgumentException {
+        bookDto.setId(id);
+        return bookService.update(bookDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookService.deleteById(id);
     }
 }
