be6f2792563ab06aa54cf8dc1f9348e23de269b8
Ard57 <grunvald.by@gmail.com>
2023-08-05 19:02:19 +0300

    Add handling for WrongLoginInfoException

--- src/main/java/com/vitenko/bookstore/web/controller/ExceptionAdvice.java
+++ src/main/java/com/vitenko/bookstore/web/controller/ExceptionAdvice.java
@@ -6,10 +6,7 @@ import com.vitenko.bookstore.exception.book.BookNotFoundException;
 import com.vitenko.bookstore.exception.cart.CartException;
 import com.vitenko.bookstore.exception.order.IllegalOrderArgumentException;
 import com.vitenko.bookstore.exception.order.OrderNotFoundException;
-import com.vitenko.bookstore.exception.user.UserEmailNotUniqueException;
-import com.vitenko.bookstore.exception.user.UserEmailWasNotProvidedException;
-import com.vitenko.bookstore.exception.user.UserNotFoundException;
-import com.vitenko.bookstore.exception.user.UserPasswordNotProvidedException;
+import com.vitenko.bookstore.exception.user.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -56,6 +53,13 @@ public class ExceptionAdvice {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleUserPasswordNotProvidedException(Model model, UserPasswordNotProvidedException e) {
+        model.addAttribute("message", e.getMessage());
+        return "error";
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleWrongLoginInfoException(Model model, WrongLoginInfoException e) {
         model.addAttribute("message", e.getMessage());
         return "error";
     }
